e6bfbcac29b72df2ddbf5a07c9bcf9ad27a4a619
zackmoss <zackmoss@students.cs.ubc.ca>
2021-11-07 16:16:51 -0800

    data again

--- Data_Transformation_Script_Billboard.R
+++ Data_Transformation_Script_Billboard.R
@@ -6,23 +6,22 @@ rawdata <- tidytuesdayR::tt_load('2021-09-14')
 audio <- rawdata$audio_features
 billboard <- rawdata$billboard
 
+billboard_singles <- billboard[0,]
 unique_songs <- unique(billboard$song_id)
-empty_matrix <- matrix(NA, nrow = length(unique_songs), ncol = ncol(billboard))
-billboard_singles <- as.data.frame(empty_matrix)
-colnames(billboard_singles) <- names(billboard)
 for(i in 1:length(unique_songs)){
   versions <- filter(billboard, song_id == unique_songs[i])
   min_peak_idx <- which.min(versions$peak_position)
-  billboard_singles[i,] <- versions[min_peak_idx,]
+  billboard_singles <- rbind(billboard_singles, versions[min_peak_idx,])
 }
 
 
 billboard_filtered <- select(billboard_singles, -c("song", "performer"))
 joined <- merge(billboard_filtered, audio, by="song_id")
-joined <- joined %>% select(-c("song_id", "url", "instance", "week_id", 
-                                "spotify_track_id", "spotify_track_preview_url", 
-                                "week_position", "previous_week_position"))
-
+joined <- joined %>% select(-c("song_id", "url", "instance", 
+                               "spotify_track_id", "spotify_track_preview_url", 
+                               "week_position", "previous_week_position"))
+joined$date <- as.Date(joined$week_id, "%m/%d/%y")
+joined  <-  joined %>% select(-c("week_id"))
 joined <- joined %>% drop_na()
 write.csv(joined, "joined_billboard_audiofeature.csv")
 set.seed(406)
@@ -32,5 +31,3 @@ training_set <- joined[training_idxs,]
 test_set <- joined[-training_idxs,]
 write.csv(training_set, "training.csv")
 write.csv(test_set, "test.csv")
-
-
